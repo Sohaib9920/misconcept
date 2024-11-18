73e2d1ab760b4a1bb12d78a4551421d8af58c41b
Sohaib9920 <sohaib9920.ahmed@gmail.com>
2024-11-18 05:41:32 +0500

    eval on both

--- trainer.py
+++ trainer.py
@@ -127,18 +127,17 @@ class Trainer:
 
     
     def evaluate(self):
-        if hasattr(self.model, "module"): # Running eval on one gpu without synch.
-            model = self.model.module
+        self.model.eval()
 
         if self.eval_val:
             print('\n{}[{}| {}]{}'.format(30*'-', self.config.rank, 'Evaluate (Val)', 30*'-'))
-            f, p, r = evaluate_eval(model, self.eval_loader_topic, self.eval_loader_content, self.topic2content,
+            f, p, r = evaluate_eval(self.model, self.eval_loader_topic, self.eval_loader_content, self.topic2content,
                                     margin=self.config.margin, fp16=self.config.fp16, bf16=self.config.bf16, 
                                     max_contents=self.config.max_contents)
         
         if self.eval_train:
             print('\n{}[{}| {}]{}'.format(30*'-', self.config.rank, 'Evaluate (Train)', 30*'-'))
-            missing_pairs, topic2wrong = evaluate_train(model, self.train_loader_topic, self.train_loader_content, self.topic2content,
+            missing_pairs, topic2wrong = evaluate_train(self.model, self.train_loader_topic, self.train_loader_content, self.topic2content,
                                                         self.content2topic, margin=self.config.margin, fp16=self.config.fp16, bf16=self.config.bf16, 
                                                         max_contents=self.config.max_contents)
                                                             
@@ -153,8 +152,7 @@ class Trainer:
             print('\n{}[{}| Epoch: {}]{}'.format(30*'-', self.config.rank, epoch, 30*'-'))
             epoch_loss = self.train_epoch()
             print('{}| Epoch: {}, Train Loss = {:.3f}, Lr = {:.6f}'.format(self.config.rank, epoch, epoch_loss, self.optimizer.param_groups[0]['lr']))
-            if self.config.rank == 0:
-                self.evaluate()
+            self.evaluate()
             dist.barrier()
 
         print(f"{self.config.rank}: End")
