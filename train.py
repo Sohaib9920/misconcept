d5ae41d16af2538a9a3e8eabe723fb7e3d2c87dd
Sohaib9920 <sohaib9920.ahmed@gmail.com>
2024-11-27 01:59:07 +0500

    small batch

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ class Configuration:
     # Training 
     seed: int = 42
     epochs: int = 1
-    train_batch_size: int = 512
+    train_batch_size: int = 32
     gradient_checkpointing: bool = True 
     weight_decay = 0.01
 
