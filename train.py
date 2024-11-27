b9a4643f7f1e840b0068a5974609f68b611d6e1b
Sohaib9920 <sohaib9920.ahmed@gmail.com>
2024-11-27 06:47:57 +0500

    validate

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ class Configuration:
 
     # Others
     fp16: bool = False
-    bf16: bool = True
+    bf16: bool = False
     
     # Debugging
     debug = True                     
@@ -41,7 +41,7 @@ class Configuration:
     weight_decay = 0.01
 
     # model_config
-    torch_dtype = torch.bfloat16
+    torch_dtype = torch.float32
     # Only use quantization when model size is very huge because when it is
     # small and use_reentrant=False, then net gain is less then only using LORA
     load_in_8bit = False
